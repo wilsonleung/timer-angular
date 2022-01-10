efec80ac8617faee6c5f4dba193f763f47d12b76
Wilson Leung <wilsonpsleung@gmail.com>
2022-01-10 18:14:19 +0800

    refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'timer-angular';
   selectedTarget = 'timer';
 
   constructor(private router: Router) {}
