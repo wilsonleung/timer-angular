66b1f642b844ce8490c7e3fae512e564b3ef75a7
Wilson Leung <wilsonpsleung@gmail.com>
2022-01-10 16:50:43 +0800

    add modal component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AnchorComponent } from './anchor/anchor.component';
 import { ButtonComponent } from './button/button.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [AnchorComponent, ButtonComponent],
-  exports: [AnchorComponent, ButtonComponent],
+  declarations: [AnchorComponent, ButtonComponent, ModalComponent],
+  exports: [AnchorComponent, ButtonComponent, ModalComponent],
 })
 export class ComponentsModule {}
