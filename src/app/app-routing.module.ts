8f6a6551c2f80e22049358c45894728e529a2386
Wilson Leung <wilsonpsleung@gmail.com>
2022-01-10 11:35:18 +0800

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { TimerComponent } from './pages/timer/timer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'timer', component: TimerComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '**', redirectTo: '/timer', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimerComponent } from './pages/timer/timer.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TimerComponent, SettingsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
