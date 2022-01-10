addf71ae390660390bf4d089657e2a0a585f743c
Wilson Leung <wilsonpsleung@gmail.com>
2022-01-10 18:14:29 +0800

    add countdown feature

--- src/app/pages/timer/timer.component.ts
+++ src/app/pages/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AppStoreProviderService } from 'src/app/utils/AppStoreProvider.service';
 
@@ -9,21 +9,28 @@ const oneSecond = 1000;
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css'],
 })
-export class TimerComponent implements OnInit, OnDestroy {
+export class TimerComponent implements OnDestroy {
   subscription: Subscription;
+  duration: number = 0;
+  showMsg = false;
   counting = false;
   countDownString = '';
+  cancel: number = -1;
 
   constructor(private appStore: AppStoreProviderService) {
     this.subscription = this.appStore.getDuration().subscribe((duration) => {
+      this.duration = duration;
       this.countDownString = this.toCountDownString(duration * 60 * oneSecond);
     });
   }
 
-  ngOnInit(): void {}
   ngOnDestroy(): void {
     if (this.subscription) {
       this.subscription.unsubscribe();
+    }
+
+    if (this.cancel) {
+      window.clearInterval(this.cancel);
     }
   }
 
@@ -34,5 +41,42 @@ export class TimerComponent implements OnInit, OnDestroy {
       2,
       '0'
     )}`;
+  }
+
+  startCountDown() {
+    console.log('[startCountDown] ...');
+
+    this.counting = true;
+    this.countDownString = this.toCountDownString(
+      this.duration * 60 * oneSecond
+    );
+
+    let tempDuration = this.duration * 60 * oneSecond;
+    this.cancel = window.setInterval(() => {
+      tempDuration = tempDuration - oneSecond;
+      this.countDownString = this.toCountDownString(tempDuration);
+
+      if (tempDuration === 0) {
+        window.clearInterval(this.cancel);
+        this.counting = false;
+        this.showMsg = true;
+      }
+    }, 1000);
+  }
+
+  stopCountDown() {
+    console.log('[stopCountDown] ...');
+    this.counting = false;
+    if (this.cancel) {
+      window.clearInterval(this.cancel);
+    }
+  }
+
+  closeMsg() {
+    this.showMsg = false;
+    this.counting = false;
+    this.countDownString = this.toCountDownString(
+      this.duration * 60 * oneSecond
+    );
   }
 }
